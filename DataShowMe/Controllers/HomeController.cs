52cab45af1edb06762cc07cb764a511713afbc58
david <davidguo.developer@outlook.com>
2014-07-10 20:59:50 -0700

    1.0 release

---
+++ DataShowMe/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataShowMe.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Data Show Me Home Page";
+            return View();
+        }
+
+    }
+}
